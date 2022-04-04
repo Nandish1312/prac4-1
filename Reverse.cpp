47d66176078e34bfa815a680b4615148a9427b34
Nandish1312 <76993210+Nandish1312@users.noreply.github.com>
2022-04-04 22:16:31 +0930

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -4,6 +4,9 @@
 
 Reverse::Reverse()
 {}
+
+
+
 string Reverse::reverseString(string letters)
 {
  size_t all_numbers= letters.size();
@@ -13,22 +16,8 @@ if(all_numbers== 1)
 return letters;
 }
 else
-{
- return letters[all_numbers- 1]+reverseString(letters.substr(0, all_numbers- 1));
-}
+
 }
 
 
 
-int Reverse::reverseDigit(int value)
-{
-if(value<10) 
-{
-return value;
-}
-int lamba=0;
-for (int ab=lamba; ab; ab/=10) lamba++;
-{
-return value%10*(int)pow(10, lamba-1) + reverseDigit(value/10);
-}
-}
