e0fc6318b779efc6112551f2b02995cff0e5b64f
Nandish1312 <76993210+Nandish1312@users.noreply.github.com>
2022-04-04 22:08:20 +0930

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,45 +1,34 @@
-#include <iostream>
-#include "Reverse.h"
-#include <string>
-#include <typeinfo>
 
-Reverse::Reverse(){}
+#include "Reverse.h"
+#include <iostream>
+
+Reverse::Reverse()
+{}
+string Reverse::reverseString(string letters)
+{
+ size_t all_numbers= letters.size();
+
+if(all_numbers== 1)
+{
+return letters;
+}
+else
+{
+ return letters[all_numbers- 1]+reverseString(letters.substr(0, all_numbers- 1));
+}
+}
+
+
 
 int Reverse::reverseDigit(int value)
 {
-
-    if(value>0)
-    {
-
-        int remainder=(value%10);
-        reversed=(reversed*10)+remainder;
-        reverseDigit(value/10);
-    }
-    else
-    return -1;
-    return reversed;
-}
-
-std::string Reverse::reverseString(std::string letters)
+if(value<10) 
 {
-
-    int length_string=letters.length();
-    if (length_string==1)
-    {
-        return letters;
-    }
-    else if(i<(length_string/2))
-    {
-        char temp=letters[i];
-        letters[i]=letters[length_string-i-1];
-        letters[length_string-i-1]=temp;
-        i++;
-        //std::cout<<i<<" "<<letters<<" "<<std::endl;
-        letters=reverseString(letters);
-    }
-    //else if(typeof(letters)!=std::string)
-    //{return "ERROR";}
-
-
-    return letters;
+return value;
+}
+int lamba=0;
+for (int ab=lamba; ab; ab/=10) lamba++;
+{
+return value%10*(int)pow(10, lamba-1) + reverseDigit(value/10);
+}
 }
